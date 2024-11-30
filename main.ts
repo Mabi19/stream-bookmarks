30be69e60b8899a68aa812d66cb4033b9733b882
Mabi19 <mateuszbartnik33@gmail.com>
2024-11-30 18:57:36 +0100

    Remove TODOs

--- main.ts
+++ main.ts
@@ -3,8 +3,3 @@ import { createApp } from "./server.ts";
 const app = createApp();
 
 Deno.serve(app.fetch);
-
-// TODO:
-// Make a test client
-// Test bookmark creation
-// Make a bookmark list per video
